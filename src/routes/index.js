dc3e4bab2b08902278be5254aaf153f1569f24c4
john <johnmendozauni@gmail.com>
2019-04-27 12:52:10 -0500

    Trece update

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('desafios');
 });
 
-router.get('/about', (req, res) => {
-  res.render('index');
+router.get('/desafios', (req, res) => {
+  res.render('desafios');
 });
 
 module.exports = router;
